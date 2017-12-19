7b3da0208ae860cbb8790b820a5ebc6c846a522f
SabariKannanM <34641045+SabariKannanM@users.noreply.github.com>
2017-12-19 10:54:00 +0530

    Update 1.c

--- Code Kata/Beginner/SET-1/1.c
+++ Code Kata/Beginner/SET-1/1.c
@@ -1,1 +1,8 @@
-
+#include<stdio.h>
+int main(void)
+{
+	int n;
+	printf("enter a number");
+	scanf("%d", &n);
+	printf("\n%d %s", n, n!=0 ? n>0 ? "is a positive number" : "is a negative number" : "is zero");
+}
