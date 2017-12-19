3bca9a5a0d6bab4a5105fd078696fdae02e43840
SabariKannanM <34641045+SabariKannanM@users.noreply.github.com>
2017-12-19 14:34:22 +0530

    Update 8 sum of natural numbers.c

--- Code Kata/Beginner/SET-1/8 sum of natural numbers.c
+++ Code Kata/Beginner/SET-1/8 sum of natural numbers.c
@@ -5,12 +5,8 @@
 #include<stdio.h>
 int main(void)
 {
-	int n1,n2=0;
+	int n;
 	printf("enter a natural number: ");
-	scanf("%d", &n1);
-	for(int i=1; i<=n1; i++)
-	{
-		n2+=i;
-	}
-		printf("\nsum of natural numbers: %d", n2);
+	scanf("%d", &n);
+	printf("\nsum of natural numbers: %d", n*(n+1)/2);
 }
