34573500cc350dfb601a6a28c3b3991133fb6857
SabariKannanM <34641045+SabariKannanM@users.noreply.github.com>
2017-12-19 11:04:05 +0530

    Update 2.c

--- Code Kata/Beginner/SET-1/2.c
+++ Code Kata/Beginner/SET-1/2.c
@@ -1,1 +1,13 @@
+/*
+odd or even number
+*/
 
+#include<stdio.h>
+int main(void)
+{
+	int n,rem;
+	printf("enter a number");
+	scanf("%d", &n);
+	rem = n%2;
+	printf("\n%d %s", n, rem==0 ? "is an even number" : "is an odd number");
+}
