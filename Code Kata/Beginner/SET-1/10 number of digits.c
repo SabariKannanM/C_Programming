67e222cc126d53953ea61ea6fb4bf33be9d15234
SabariKannanM <34641045+SabariKannanM@users.noreply.github.com>
2017-12-19 14:39:47 +0530

    Create 10 number of digits.c

---
+++ Code Kata/Beginner/SET-1/10 number of digits.c
@@ -0,0 +1,12 @@
+/*
+10 - Count number of digits of an integer.
+*/
+
+#include<stdio.h>
+int main(void)
+{
+	int n,digits;
+	printf("enter a natural number: ");
+	scanf("%d", &n);
+	printf("\nNumber of digits: %d", printf("%d", n));
+}
