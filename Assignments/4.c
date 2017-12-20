ad73979bd84b3e8fd05bff8a48ee4815bf842e14
SabariKannanM <34641045+SabariKannanM@users.noreply.github.com>
2017-12-20 16:31:22 +0530

    Update 4.c

--- Assignments/4.c
+++ Assignments/4.c
@@ -1,1 +1,17 @@
+/*
+rotate an array 
+i/p : n=5, k=2
+o/p : 30 40 50 10 20 
+*/
+#include <stdio.h>
 
+int main(void) {
+	int a[10], n ,k;
+	scanf("%d %d", &n, &k);
+	for(int i=1, j=0; i<=n; i++, j++)
+	a[j] = i*10;
+	for(int i=k; i<n; i++)
+	printf("%d ", a[i]);
+	for(int i=0; i<k; i++)
+	printf("%d ", a[i]);
+}
